1d8ed28aa2a1a78adfe32789bae2bc15f0ee741c
chiachih.wu <asd010170@gmail.com>
2024-06-23 15:07:41 +0900

    従業員destroyアクションの作成

--- src/main/java/services/EmployeeService.java
+++ src/main/java/services/EmployeeService.java
@@ -166,7 +166,7 @@ public class EmployeeService extends ServiceBase {
         savedEmp.setUpdatedAt(today);
 
         // 論理削除フラグを立てる
-        savedEmp.setDelectFlag(JpaConst.EMP_DEL_TRUE);
+        savedEmp.setDeleteFlag(JpaConst.EMP_DEL_TRUE);
 
         // 更新処理を行う
         update(savedEmp);
